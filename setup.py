86ad1c77f273a91da87e72cd8f89eae7a0ed0d7d
Vu Anh <brother.rain.1024@gmail.com>
2017-03-02 11:38:16 +0700

    update version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requirements = [
 
 setup(
     name='underthesea',
-    version='1.0.0',
+    version='1.0.1',
     description="Vietnamese NLP Toolkit",
     long_description=readme + '\n\n' + history,
     author="Vu Anh",
