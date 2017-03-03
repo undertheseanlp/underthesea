de35d2d00a02acf05e7d8c5de97613d06fa4aaeb
Doan Viet Dung <doanvietdung273@gmail.com>
2017-03-03 15:00:25 +0700

    add corpus

--- tests/corpus/test_plaintext.py
+++ tests/corpus/test_plaintext.py
@@ -8,3 +8,7 @@ class TestPlainTextCorpus(TestCase):
         corpus = PlainTextCorpus()
         self.assertIsNone(corpus.documents)
 
+    def test_load(self):
+        corpus = PlainTextCorpus()
+        corpus.load("sample_text_corpus")
+        self.assertEqual(4, len(corpus.documents))
--- underthesea/corpus/plaintext.py
+++ underthesea/corpus/plaintext.py
@@ -1,13 +1,24 @@
 from corpus import Corpus
+from os.path import join
+from os import listdir
 
 
 class PlainTextCorpus(Corpus):
-    """corpus for handling plain text
+    """class for handling plain text corpus
     """
+
     def __init__(self):
         self.documents = None
 
     def load(self, folder):
+        """load plaintext folder to documents
+
+        :param folder path to directory
+        :type folder: string
+        """
+        files = listdir(folder)
+        files = [join(folder, f) for f in files]
+        self.documents = [open(f, "r").read() for f in files]
         pass
 
     def save(self, folder):
