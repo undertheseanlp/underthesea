6d7e4b25d90710072089435f6982b0fdd6335515
Doan Viet Dung <doanvietdung273@gmail.com>
2017-03-28 13:37:07 +0700

    delete format list

--- underthesea/corpus/word_sent/transformer.py
+++ underthesea/corpus/word_sent/transformer.py
@@ -22,39 +22,6 @@ class Transformer:
     def extract_features(sentence):
         return sent2features(sentence)
 
-    def format_word(self, sentence):
-        path = join(dirname(__file__), "punctuation.txt")
-        punctuations = open(path, "r").read().split("\n")
-        for punctuation in punctuations:
-            punctuation = unicode(punctuations)
-        words = []
-        for word in sentence.split(" "):
-            if "_" in word:
-                tokens = []
-                word = word.replace("_", " ")
-                for token in word.split(" "):
-                    if token != "":
-                        tokens.append(token)
-
-                for i in range(tokens.__len__()):
-                    if i != 0:
-                        tokens[i] += "\tI_W"
-                    else:
-                        tokens[i] += "\tB_W"
-                    words.append(tokens[i])
-            elif word in punctuations:
-                words.append(word + "\tO")
-            else:
-                words.append(word + "\tB_W")
-        return words
-
-    def list_to_tuple(self, sentences):
-        word_tuple = []
-        for i in sentences:
-            arr = i.split('\t')
-            word_tuple.append((arr[0], arr[1]))
-        return word_tuple
-
     def load_train_sents(self):
         corpus = PlainTextCorpus()
         file_path = join(dirname(dirname(dirname(__file__))), "data", "corpus_2", "train", "input")
