da761304de95af6b91db0bdd71927c41b23739cb
Vu Anh <brother.rain.1024@gmail.com>
2017-05-09 15:58:21 +0700

    update tokenize

--- underthesea/word_sent/tokenize.py
+++ underthesea/word_sent/tokenize.py
@@ -12,12 +12,14 @@ def tokenize(text):
     digit = "\d+([\.,_]\d+)+"
     email = "(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
     web = "^(http[s]?://)?(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+$"
+    datetime = "\d{,2}\/\d{,2}\/\d{,4}"
     word = "\w+"
     non_word = "[^\w\s]"
 
     patterns = []
     patterns.extend(specials)
     patterns.extend([web, email])
+    patterns.extend([datetime])
     patterns.extend([digit, non_word, word])
 
     patterns = "(" + "|".join(patterns) + ")"
