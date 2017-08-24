7c967fdd3e39e6e8b2a290f891958d5198749a0a
rain <brother.rain.1024@gmail.com>
2017-08-24 16:29:16 +0700

    v1.1.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='underthesea',
-    version='1.1.1',
+    version='1.1.2',
     description="Vietnamese NLP Toolkit",
     long_description=readme + '\n\n' + history,
     author="Vu Anh",
