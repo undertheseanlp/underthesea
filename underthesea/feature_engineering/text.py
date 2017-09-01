ee63be9c15f38b175364f25c8f124cc017203feb
rain <brother.rain.1024@gmail.com>
2017-09-01 10:53:44 +0700

    fix text

--- underthesea/feature_engineering/text.py
+++ underthesea/feature_engineering/text.py
@@ -5,12 +5,13 @@ import unicodedata
 
 def Text(text):
     """ provide a wrapper for python string
+    map byte to str (python 3)
+    map str to unicode (python 2)
+    all string in utf-8 encoding
+    normalize string to NFC
     """
-    if sys.version_info >= (3, 0):
-        unicode = str
-    else:
-        if not is_unicode(text):
-           text = text.decode("utf-8")
+    if not is_unicode(text):
+       text = text.decode("utf-8")
     text = unicodedata.normalize("NFC", text)
     return text
 
