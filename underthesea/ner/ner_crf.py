add032663f3d6e5cd3475dde346e8856b945cc2c
rain <brother.rain.1024@gmail.com>
2017-10-06 17:55:08 +0700

    add tests"

--- underthesea/ner/ner_crf.py
+++ underthesea/ner/ner_crf.py
@@ -9,7 +9,7 @@ from underthesea.util.singleton import Singleton
 class NERCRFModel:
     def __init__(self):
         self.model = pycrfsuite.Tagger()
-        filepath = join(dirname(__file__), "ner_crf_20171005.model")
+        filepath = join(dirname(__file__), "ner_crf_20171006_template_2.model")
         self.model.open(filepath)
 
     def predict(self, text, format=None):
@@ -41,8 +41,22 @@ class Transformer:
             # ner
             "T[-3][3]", "T[-2][3]", "T[-1][3]",
         ]
+        template2 = [
+            "T[-2].lower", "T[-1].lower", "T[0].lower", "T[1].lower",
+            "T[2].lower",
+            "T[0].istitle", "T[-1].istitle", "T[1].istitle", "T[-2].istitle",
+            "T[2].istitle",
+            # word unigram and bigram
+            "T[-2]", "T[-1]", "T[0]", "T[1]", "T[2]",
+            "T[-2,-1]", "T[-1,0]", "T[0,1]", "T[1,2]",
+            # pos unigram and bigram
+            "T[-2][1]", "T[-1][1]", "T[0][1]", "T[1][1]", "T[2][1]",
+            "T[-2,-1][1]", "T[-1,0][1]", "T[0,1][1]", "T[1,2][1]",
+            # ner
+            "T[-3][3]", "T[-2][3]", "T[-1][3]",
+        ]
         sentence = [(token[0], token[1], token[2], "X") for token in sentence]
-        return sent2features(sentence, template)
+        return sent2features(sentence, template2)
 
     @staticmethod
     def extract_features(sentence, template):
