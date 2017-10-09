58a864757eae065d2248b76953f5dfe10b6357a5
rain <brother.rain.1024@gmail.com>
2017-10-09 16:33:52 +0700

    add word_sent_2

--- tests/word_sent/test_word_sent.py
+++ tests/word_sent/test_word_sent.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
-from os import listdir
+from os import listdir, mkdir
 from unittest import TestCase
+
+import shutil
+
 from underthesea import word_sent
-from os.path import dirname, join
+from underthesea import word_sent_2
+from os.path import dirname, join, isfile
 from underthesea.util.file_io import read, write
 
 
@@ -48,3 +52,40 @@ class TestWord_sent(TestCase):
                 print("Fail {}".format(id))
                 save_temp(id, actual)
             self.assertEqual(actual, expected)
+
+    def _save_tmp_model(self, model_id, file_id, output):
+        test_dir = join(dirname(__file__), "test_set", model_id)
+        temp_file = join(test_dir, "%s.actual" % file_id)
+        content = u"\n".join(output)
+        write(temp_file, content)
+
+    def _test_model(self, model_id, model):
+        name = model.__module__
+        print(name)
+        test_dir = join(dirname(__file__), "test_set")
+        files = [f for f in listdir(test_dir) if isfile(join(test_dir, f))]
+        ids = sorted([int(f.split(".")[0]) for f in files if ".in" in f])
+        fails = []
+        try:
+            shutil.rmtree(join(test_dir, model_id))
+        except:
+            pass
+        mkdir(join(test_dir, model_id))
+        for id in ids:
+            input_file = join(test_dir, "%s.in" % id)
+            output_file = join(test_dir, "%s.out" % id)
+            sentence = load_input(input_file)
+            actual = word_sent(sentence)
+            expected = load_output(output_file)
+            if actual != expected:
+                fails.append(str(id))
+                self._save_tmp_model(model_id, id, actual)
+        n = len(ids)
+        correct = n - len(fails)
+        print("Accuracy: {:.2f}% ({}/{})".format(correct * 100.0 / n, correct, n))
+        print("Fails   :", ", ".join(fails))
+
+    def test_models(self):
+        models = [word_sent, word_sent_2.word_sent]
+        for i, model in enumerate(models):
+            self._test_model(str(i + 1), model)
