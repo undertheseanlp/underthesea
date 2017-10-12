0334fa84aa95a998aa038f7499142cfc1fd4111c
rain <brother.rain.1024@gmail.com>
2017-10-12 10:06:21 +0700

    retrain word_sent, pos_tag, chunking

---
+++ underthesea/chunking/__init__.py
@@ -0,0 +1,16 @@
+from underthesea import pos_tag
+import sys
+
+if sys.version_info >= (3, 0):
+    from .model_crf import CRFChunkingPredictor
+else:
+    from model_crf import CRFChunkingPredictor
+
+
+
+def chunk(sentence):
+    sentence = pos_tag(sentence)
+    crf_model = CRFChunkingPredictor.Instance()
+    result = crf_model.predict(sentence, format)
+    return result
+
