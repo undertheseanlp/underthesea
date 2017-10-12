47c5340e7eec642172b5a47f00bb0e779ed2c254
rain <brother.rain.1024@gmail.com>
2017-10-12 11:47:34 +0700

    change config to one_line_install

--- underthesea/__init__.py
+++ underthesea/__init__.py
@@ -7,17 +7,21 @@ __version__ = '1.1.4'
 import sys
 
 if sys.version_info >= (3, 0):
-
     from underthesea.word_sent import word_sent
     from underthesea.pos_tag import pos_tag
     from underthesea.chunking import chunk
-
-    from underthesea.classification import classify
+    try:
+        from underthesea.classification import classify
+    except:
+        pass
 else:
     from word_sent import word_sent
     from pos_tag import pos_tag
     from chunking import chunk
-    from classification import classify
+    try:
+        from classification import classify
+    except:
+        pass
 
 
 def info(version):
