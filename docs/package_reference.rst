7a29b8b86ae6b5b7c60321f9dad32a3a44f7d24e
jackNhat <buinhatanh1208@gmail.com>
2017-12-25 15:31:40 +0700

    update doc

--- docs/package_reference.rst
+++ docs/package_reference.rst
@@ -5,3 +5,7 @@ word_sent
 .. automodule:: underthesea.word_sent
 
 .. autofunction:: underthesea.word_sent.word_sent
+
+.. automodule:: underthesea.pos_tag
+
+.. autofunction:: underthesea.pos_tag.pos_tag
--- underthesea/pos_tag/__init__.py
+++ underthesea/pos_tag/__init__.py
@@ -3,6 +3,20 @@ from .model_crf import CRFPOSTagPredictor
 
 
 def pos_tag(sentence, format=None):
+    """
+    Vietnamese POS tagging
+
+    Parameters
+    ==========
+
+    sentence: {unicode, str}
+        Raw sentence
+
+    Returns
+    =======
+    tokens: list of text
+        tagged sentence
+    """
     sentence = word_sent(sentence)
     crf_model = CRFPOSTagPredictor.Instance()
     result = crf_model.predict(sentence, format)
