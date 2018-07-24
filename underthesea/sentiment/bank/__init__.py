995682e2cb0699a39bdb0f7d2f8f5cc77f9cc6fb
anhv <anhv.ict91@gmail.com>
2018-07-24 21:53:23 +0700

    fix load model bank_sentiment

--- underthesea/sentiment/bank/__init__.py
+++ underthesea/sentiment/bank/__init__.py
@@ -1,14 +1,23 @@
 import joblib
 from os.path import join, dirname
 import sys
+
 sys.path.insert(0, dirname(__file__))
 
-y_transform = joblib.load(join(dirname(__file__), "label.transformer.bin"))
-x_transform = joblib.load(join(dirname(__file__), "count.transformer.bin"))
-estimator = joblib.load(join(dirname(__file__), "model.bin"))
+bank_sentiment = {}
 
 
 def sentiment(X):
+    global bank_sentiment
+    if "x_transform" not in bank_sentiment:
+        bank_sentiment["x_transform"] = joblib.load(join(dirname(__file__), "count.transformer.bin"))
+    if "y_transform" not in bank_sentiment:
+        bank_sentiment["y_transform"] = joblib.load(join(dirname(__file__), "label.transformer.bin"))
+    if "estimator" not in bank_sentiment:
+        bank_sentiment["estimator"] = joblib.load(join(dirname(__file__), "model.bin"))
+    x_transform = bank_sentiment["x_transform"]
+    y_transform = bank_sentiment["y_transform"]
+    estimator = bank_sentiment["estimator"]
     if isinstance(X, list):
         return y_transform.inverse_transform(
             estimator.predict(x_transform.transform(X)))
