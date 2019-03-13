10a295924f2b56bbe0f310291ef4ea7f2e02210d
Vu Anh <anhv.ict91@gmail.com>
2019-03-13 12:22:47 +0700

    GH-226: add info command

--- underthesea/cli.py
+++ underthesea/cli.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import click
+import platform
 from underthesea.util import download_component
 from underthesea.util.data import download_default_components
 
@@ -19,6 +20,27 @@ def download(component):
 @main.command()
 def data():
     download_default_components()
+
+@main.command()
+def info():
+    underthesea_version = open("VERSION").read().strip()
+    python_version = platform.python_version()
+    system_info = f"{platform.system()}-{platform.release()}"
+    print("")
+    print("ENVIRONMENT")
+    print(f" underthesea version : {underthesea_version}")
+    print(f"      python version : {python_version}")
+    print(f"  system information : {system_info}")
+
+    print("")
+    print("MODULES")
+    print("       sent_tokenize : OK")
+    print("       word_tokenize : OK")
+    print("             pos_tag : OK")
+    print("               chunk : OK")
+    print("                 ner : OK")
+    print("            classify : OK")
+    print("           sentiment : OK")
 
 
 if __name__ == "__main__":
