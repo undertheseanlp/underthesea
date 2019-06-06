4076f1260c3170a7a2e1f01081e0fc4405c7b827
QuangHPham <hongquang2310255@gmail.com>
2019-06-06 22:00:56 +0700

    GH-243: update UTS2017_BANK_TC model

--- underthesea/classification/bank/__init__.py
+++ underthesea/classification/bank/__init__.py
@@ -1,26 +1,15 @@
-import joblib
 from os.path import join, dirname
 import sys
 
-sys.path.insert(0, dirname(__file__))
+from languageflow.data import Sentence
+from languageflow.models.text_classifier import TextClassifier
 
-bank_classification = {}
+sys.path.insert(0, dirname(__file__))
 
 
 def classify(X):
-    global bank_classification
-    if "x_transform" not in bank_classification:
-        bank_classification["x_transform"] = joblib.load(join(dirname(__file__), "tfidf.transformer.bin"))
-    if "y_transform" not in bank_classification:
-        bank_classification["y_transform"] = joblib.load(join(dirname(__file__), "label.transformer.bin"))
-    if "estimator" not in bank_classification:
-        bank_classification["estimator"] = joblib.load(join(dirname(__file__), "model.bin"))
-    x_transform = bank_classification["x_transform"]
-    y_transform = bank_classification["y_transform"]
-    estimator = bank_classification["estimator"]
-    if isinstance(X, list):
-        return y_transform.inverse_transform(
-            estimator.predict(x_transform.transform(X)))
-    else:
-        return y_transform.inverse_transform(
-            estimator.predict(x_transform.transform([X])))[0]
+    classifer = TextClassifier.load(join(dirname(__file__), 'classification_svm_uts2017_bank'))
+    sentence = Sentence(X)
+    classifer.predict(sentence)
+    label = sentence.labels
+    return label
