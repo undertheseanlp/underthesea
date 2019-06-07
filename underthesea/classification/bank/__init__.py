a7ecd8fe4743fbc7a95a56440e7a15a34959899d
QuangHPham <hongquang2310255@gmail.com>
2019-06-07 23:40:43 +0700

    GH-243: update UTS2017_BANK_TC multilabel model

--- underthesea/classification/bank/__init__.py
+++ underthesea/classification/bank/__init__.py
@@ -6,11 +6,12 @@ from languageflow.models.text_classifier import TextClassifier
 
 sys.path.insert(0, dirname(__file__))
 
-classifer = TextClassifier.load(join(dirname(__file__), 'classification_svm_uts2017_bank'))
+classifer = TextClassifier.load(join(dirname(__file__), 'tc_svm_uts2017_bank_20190607'))
 
 
 def classify(X):
     sentence = Sentence(X)
     classifer.predict(sentence)
-    label = sentence.labels
-    return label
+    labels = sentence.labels
+    label_values = [label.value for label in labels]
+    return label_values
