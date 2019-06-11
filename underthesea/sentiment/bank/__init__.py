c8a035f60646e80e6b14e04ad58c98cac0106378
Suong <phamsuong1551997@gmail.com>
2019-06-11 22:12:38 +0700

    GH-244: Update UTS2017_BANK_SA model

--- underthesea/sentiment/bank/__init__.py
+++ underthesea/sentiment/bank/__init__.py
@@ -1,12 +1,13 @@
 from os.path import join, dirname
 import sys
 from languageflow.data import Sentence
-from languageflow.models.text_classifier import TextClassifier
+from underthesea.model_fetcher import ModelFetcher, UTSModel
+
 
 sys.path.insert(0, dirname(__file__))
 
-model_folder = 'sentiment_svm_uts2017_bank_sa'
-classifier = TextClassifier.load(join(dirname(__file__), model_folder))
+model_folder = 'se_svm_2019-06'
+classifier = ModelFetcher.load_model(UTSModel.se_svm_bank_2019_06)
 
 
 def sentiment(text):
