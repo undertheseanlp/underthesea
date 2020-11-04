73af1d0348245c4b2332c9559f883215dd230b4d
Vu Anh <anhv.ict91@gmail.com>
2020-11-04 14:45:56 +0700

    GH-299: add trainer and tagger (#336)
    * GH-299: add validate_corpus and revise_corpus

---
+++ underthesea/models/crf_sequence_tagger.py
@@ -0,0 +1,3 @@
+class CRFSequenceTagger:
+    def __init__(self, features):
+        self.features = features
---
+++ underthesea/trainers/__init__.py
@@ -0,0 +1,6 @@
+class ModelTrainer:
+    def __init__(self, tagger, corpus):
+        pass
+
+    def train(self, model_folder):
+        print(model_folder)
