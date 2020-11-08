4df13fe503bf4430d5016c462c19d815f427988a
anhv <anhv.ict91@gmail.com>
2020-11-08 18:18:24 +0700

    GH-299: remove experiments

--- underthesea/experiments/word_tokenize_predict.py
+++
@@ -1,7 +0,0 @@
-from os.path import join
-
-from underthesea.file_utils import CACHE_ROOT
-from underthesea.trainers import evaluate_
-
-base_path = "models/wtk_crf"
-evaluate_(join(CACHE_ROOT, base_path, "output.txt"))
