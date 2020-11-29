3d5727ca5770975074f4b256bc6c520b9bfb7ccb
anhv <anhv.ict91@gmail.com>
2020-11-29 23:12:06 +0700

    update default model

--- tests/dependency_parse/test_dependency_parse.py
+++ tests/dependency_parse/test_dependency_parse.py
@@ -8,14 +8,13 @@ class TestDependencyParse(TestCase):
         text = 'Tối 29/11, Việt Nam thêm 2 ca mắc Covid-19'
         actual = dependency_parse(text)
         expected = [
-            ('Tối', 8, 'punct'),
-            ('29/11', 8, 'punct'),
-            (',', 8, 'punct'),
-            ('Việt Nam', 8, 'punct'),
-            ('thêm', 8, 'punct'),
-            ('2', 8, 'punct'),
-            ('ca', 8, 'punct'),
-            ('mắc', 8, 'punct'),
-            ('Covid-19', 8, 'punct')
-        ]
+            ('Tối', 5, 'obl:tmod'),
+            ('29/11', 1, 'flat:date'),
+            (',', 1, 'punct'),
+            ('Việt Nam', 5, 'nsubj'),
+            ('thêm', 0, 'root'),
+            ('2', 7, 'nummod'),
+            ('ca', 5, 'obj'),
+            ('mắc', 7, 'nmod'),
+            ('Covid-19', 8, 'nummod')]
         self.assertEqual(actual, expected)
