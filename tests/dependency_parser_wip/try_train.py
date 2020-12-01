3d89497a43361b401c8f869e2e97409f1ad71610
Vu Anh <anhv.ict91@gmail.com>
2020-12-01 11:51:05 +0700

    update

--- tests/dependency_parser_wip/try_train.py
+++ tests/dependency_parser_wip/try_train.py
@@ -56,6 +56,6 @@ parser = DependencyParser(embeddings, embed=False)
 trainer = DependencyParserTrainer(parser, corpus)
 trainer.train(
     base_path='tmp/resources/parsers/dp-3',
-    max_epochs=2,
+    max_epochs=3,
     mu=0  # optimizer parameters
 )
