81d2c75d033440dacb3605c41874521ed8d5f4ac
anhv <anhv.ict91@gmail.com>
2020-12-28 22:29:31 +0700

    update

--- tests/classification/test_bank.py
+++ tests/classification/test_bank.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 from underthesea import classify
 
 
-class TestClassify(TestCase):
+class TestBankClassify(TestCase):
     def test_classify_simple_case(self):
         text = u"Mở tài khoản ATM thì có đc quà ko ad"
         actual = classify(text, domain='bank')
