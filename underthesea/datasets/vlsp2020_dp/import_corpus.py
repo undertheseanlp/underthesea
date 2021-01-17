79179e4540e2af99dd8b8fa2e68051a45ae26a71
anhv <anhv.ict91@gmail.com>
2021-01-17 09:31:35 +0700

    update

--- underthesea/datasets/vlsp2020_dp/import_corpus.py
+++ underthesea/datasets/vlsp2020_dp/import_corpus.py
@@ -15,13 +15,42 @@
 # -     20K    Nov  8 23:07    SA-Restaurant-50.txt
 # -    1.6M    May 12  2020    VTB_2996.txt
 # -    375K    Nov  8 23:07    VTB_400.txt
+import os
 import shutil
 from os.path import join
 from zipfile import ZipFile
 
 from underthesea.file_utils import DATASETS_FOLDER
+
 ORIGINAL_ZIP_FILE = join(DATASETS_FOLDER, 'VLSP2020-DP.zip')
 ORIGINAL_FOLDER = join(DATASETS_FOLDER, 'VLSP2020-DP-ORIGINAL')
-shutil.rmtree(ORIGINAL_FOLDER)
-with ZipFile(ORIGINAL_ZIP_FILE) as file:
-    file.extractall(ORIGINAL_FOLDER)
+DEST_FOLDER = join(DATASETS_FOLDER, 'VLSP2020-DP')
+shutil.rmtree(DEST_FOLDER)
+os.makedirs(DEST_FOLDER)
+
+
+# shutil.rmtree(ORIGINAL_FOLDER)
+# with ZipFile(ORIGINAL_ZIP_FILE) as file:
+#     file.extractall(ORIGINAL_FOLDER)
+
+class CONLLReader:
+    @staticmethod
+    def load(file):
+        sentences = open(file).read().split('\n\n')
+        sentences = [s for s in sentences if len(s) > 10]
+        return sentences
+
+    @staticmethod
+    def save(sentences, file):
+        content = '\n\n'.join(sentences) + '\n'
+        with open(file, 'w') as f:
+            f.write(content)
+
+
+files = ['DP-Package2.18.11.2020.txt', 'VTB_2996.txt', 'VTB_400.txt', 'SA-Restaurant-50.txt', 'SA-Hotel-50.txt',
+         'HTB_1570.txt']
+files = [join(DATASETS_FOLDER, ORIGINAL_FOLDER, f) for f in files]
+sentences = []
+for file in files:
+    sentences += CONLLReader.load(file)
+CONLLReader.save(sentences, join(DEST_FOLDER, 'train.txt'))
