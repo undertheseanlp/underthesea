b432c0f864d3fdfa64f81ac6261f8ff6ef9209c6
Tai D Nguyen <tainguyen7597@gmail.com>
2021-07-20 15:04:00 +0700

    update data parser

--- examples/sentiment/train.py
+++ examples/sentiment/train.py
@@ -1,4 +1,4 @@
-# CONTENT
+# Txt format:
 # #1
 # Mà không biết phải nhân viên cũ hay mới nữa nhưng cảm giác thân thiện hơn.
 # {SERVICE#GENERAL, positive}
@@ -7,6 +7,8 @@
 # Nay đi uống mới biết giá thành hơi cao nhưng thật sự đi đôi với chất lượng.
 # {RESTAURANT#PRICES, negative}, {RESTAURANT#GENERAL, positive}
 #
+
+import re
 
 fnames = ["/Users/taidnguyen/Desktop/Sentence-level-Restaurant/Dev.txt",
           "/Users/taidnguyen/Desktop/Sentence-level-Hotel/Dev_Hotel.txt"]
@@ -18,19 +20,38 @@ class ABSentimentData(str):
         lines = fin.readlines()
         fin.close()
 
-        data = []
-        labels = []
+        sentences = []
+        aspects = []
+        subaspects = []
+        tones = []
         for i in range(1, len(lines), 4):
             sentence = lines[i].replace("\n", "")
-            label = lines[i + 1].replace("\n", "")
-            data.append(sentence)
-            labels.append(label)
+            label_pattern = r"([\w#&]+)"  # keep aspect like DESIGN&FEATURES together
+            labels = re.findall(label_pattern,
+                                lines[i + 1].replace("\n", ""))  # array of 'ASPECT#SUBASPECT', 'tone',... for sentence
 
-        self.data = data
-        self.labels = labels  #aspect, sub-aspect, tone
+            aspect = []
+            subaspect = []
+            tone = []
+            for j in range(0, len(labels), 2):
+                aspect.append(labels[j].split("#")[0])
+                subaspect.append(labels[j].split("#")[1])
+                tone.append(labels[j + 1])
+            aspects.append(aspect)
+            subaspects.append(subaspect)
+            tones.append(tone)
+            sentences.append(sentence)
 
-        # print(self.data)
-        # print(self.labels)
+        assert len(sentences) == len(aspects) == len(subaspects) == len(tones), \
+            "Mismatched data while splitting:\n{0}, {1}, {2}, {3}".format(len(sentences), len(aspects), len(subaspects),
+                                                                          len(tones))
+
+        self.sentences = sentences
+        self.aspects = aspects
+        self.subaspects = subaspects
+        self.tones = tones
+
+        print(self.sentences[1], self.aspects[1], self.subaspects[1], self.tones[1])
 
 
 class Tokenizer(object):
@@ -39,6 +60,4 @@ class Tokenizer(object):
         self.max_seq_len = max_seq_len
 
 
-
-
-ABSentimentData(fnames[1])
+ABSentimentData(fnames[0])
