429f5499c357b730960fdf2e8f103bccab5a2606
anh.vu2 <anh.vu2@mservice.com.vn>
2021-07-24 19:52:47 +0700

    update

--- examples/ner/train.py
+++ examples/ner/train.py
@@ -1,4 +1,37 @@
-from transformers import pipeline
+import torch
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+from torch.nn import functional as F
+import torch.nn as nn
 
-nlp = pipeline("ner")
-print(nlp("VinFast đặt cược gì để đấu với các đối thủ tại Mỹ?"))
+
+class SequenceTokenClassifier(pl.LightningModule):
+
+    def __init__(self):
+        super().__init__()
+        self.lstm = nn.LSTM(3, 3)
+
+    def train_dataloader(self) -> DataLoader:
+        pass
+
+    def training_step(self, batch, batch_idx):
+        X, y = batch
+        y_hat = self.backbone(X)
+        loss = F.cross_entropy(y_hat, y)
+        self.log('train_loss', loss, on_epoch=True)
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(params=self.parameters())
+
+
+if __name__ == '__main__':
+    train_loader = None
+    val_loader = None
+    test_loader = None
+    model = SequenceTokenClassifier()
+
+    trainer = pl.Trainer(gpus=1)
+    trainer.fit(model=model, train_dataloader=train_loader, val_dataloaders=val_loader)
+
+    result = trainer.test(test_dataloaders=test_loader)
