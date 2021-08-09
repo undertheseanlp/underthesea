36ec232119b56b2a92749ff680f3b6b5a0f5310b
Vu Anh <anhv.ict91@gmail.com>
2021-08-09 10:00:24 +0700

    GH-424: Train model with UIT_ABSA dataset (#427)

---
+++ examples/sentiment/README.md
@@ -0,0 +1,35 @@
+# Sentiment
+
+## Train model with GPT-2
+
+```
+python train_gpt2.py data.batch_size=2 ++data.num_workers=16 logger.project=local-sentiment-5
+```
+
+Run with 100 samples
+
+```
+python train_gpt2.py data.batch_size=2 data.samples=100 logger.project=debug-sentiment-5 
+```
+
+Run in clouod
+```
+python train_gpt2.py data.batch_size=16 ++data.num_workers=16 logger.project=cloud-sentiment-5
+```
+
+## Train model with Bert
+
+```
+python train_bert.py data.batch_size=2 ++data.num_workers=16 logger.project=local-sentiment-5
+```
+
+Run with 100 samples
+
+```
+python train_bert.py data.batch_size=2 data.samples=100 logger.project=debug-sentiment-5 
+```
+
+Run in clouod
+```
+python train_bert.py data.batch_size=16 ++data.num_workers=16 logger.project=cloud-sentiment-5
+```
---
+++ underthesea/datasets/uit_absa_hotel/uit_absa_hotel.py
@@ -0,0 +1,82 @@
+from os.path import join
+import re
+from underthesea.data_fetcher import DataFetcher
+from underthesea.file_utils import DATASETS_FOLDER
+
+_CITATION = None
+
+_DESCRIPTION = None
+
+_HOMEPAGE = None
+
+_LICENSE = None
+
+
+class LabelEncoder:
+    def __init__(self):
+        self.index2label = {}
+        self.label2index = {}
+        self.vocab_size = 0
+
+    def encode(self, labels):
+        if type(labels) == list:
+            return [self.encode(label) for label in labels]
+        label = labels  # label is a string
+        if label not in self.label2index:
+            index = self.vocab_size
+            self.label2index[label] = index
+            self.index2label[index] = label
+            self.vocab_size += 1
+            return index
+        else:
+            return self.label2index[label]
+
+
+class UITABSAHotel:
+    NAME = "UIT_ABSA_HOTEL"
+    VERSION = "1.0"
+
+    def __init__(self):
+        DataFetcher.download_data(UITABSAHotel.NAME, None)
+        train_file = join(DATASETS_FOLDER, UITABSAHotel.NAME, "Train_Hotel.txt")
+        dev_file = join(DATASETS_FOLDER, UITABSAHotel.NAME, "Dev_Hotel.txt")
+        test_file = join(DATASETS_FOLDER, UITABSAHotel.NAME, "Test_Hotel.txt")
+
+        self.label_encoder = LabelEncoder()
+        self.aspect_label_encoder = LabelEncoder()
+        self.train = self._extract_sentences(train_file)
+        self.dev = self._extract_sentences(dev_file)
+        self.test = self._extract_sentences(test_file)
+        self.num_labels = self.label_encoder.vocab_size
+        self.num_aspect_labels = self.aspect_label_encoder.vocab_size
+
+    def _join_labels(self, label):
+        return "#".join(label)
+
+    def _extract_sentence(self, sentence):
+        sentence_id, text, labels = sentence.split("\n")
+        labels = re.findall("\{(?P<aspect>.*?), (?P<polarity>.*?)\}", labels)
+        aspect_polarity_labels = [self._join_labels(label) for label in labels]
+        label_ids = self.label_encoder.encode(aspect_polarity_labels)
+
+        aspect_labels = [label[0] for label in labels]
+        aspect_label_ids = self.aspect_label_encoder.encode(aspect_labels)
+        return {
+            "sentence_id": sentence_id,
+            "text": text,
+            "labels": labels,
+            "label_ids": label_ids,
+            "aspect_labels": aspect_labels,
+            "aspect_label_ids": aspect_label_ids
+        }
+
+    def _extract_sentences(self, file):
+        with open(file) as f:
+            content = f.read()
+            sentences = content.split("\n\n")
+            sentences = [self._extract_sentence(s) for s in sentences]
+        return sentences
+
+
+if __name__ == '__main__':
+    dataset = UITABSAHotel()
