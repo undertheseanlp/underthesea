9bf8e12b70c08d90934d2e4302bb4d42be88c292
Tai D Nguyen <tainguyen7597@gmail.com>
2021-08-21 20:18:40 +0700

    update

--- underthesea/utils/col_lyrics.py
+++ underthesea/utils/col_lyrics.py
@@ -25,7 +25,7 @@ def playlist_youtube_api(playlist):
 
     api_service_name = "youtube"
     api_version = "v3"
-    client_secrets_file = "../../../../../backupABSA/client_secret.json"
+    client_secrets_file = "client_secret.json"
 
     # Get credentials and create an API client
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
