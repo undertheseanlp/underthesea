50d55705444a5e31a4c883748d7a082d7a4af016
anh.vu2 <anh.vu2@mservice.com.vn>
2021-11-13 16:36:29 +0700

    update

--- extensions/underthesea_core/src/featurizers.rs
+++ extensions/underthesea_core/src/featurizers.rs
@@ -1,20 +1,16 @@
 
-fn generate_feature(_a: String) -> String {
-    let result = String::from("Chào bạn");
-    return result;
-}
-
-pub fn lower(_a: String) -> String {
-    return _a.to_lowercase();
+pub fn featurizer(sentences: Vec<String>) -> Vec<String> {
+    return sentences.iter().map(|s| s.to_lowercase()).collect();
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        let b = super::generate_feature(String::from("abc"));
-        let expected = String::from("Chào bạn");
-        assert_eq!(b, expected);
+    fn test_featurizer() {
+        let input: Vec<String> = ["A", "b"].iter().map(|&s| s.into()).collect();
+        let output = super::featurizer(input);
+        let expected: Vec<String> = ["a", "b"].iter().map(|&s| s.into()).collect();
+        assert_eq!(output, expected);
     }
 }
 
--- extensions/underthesea_core/src/lib.rs
+++ extensions/underthesea_core/src/lib.rs
@@ -1,19 +1,14 @@
 use pyo3::prelude::*;
 mod featurizers;
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
 
 #[pyfunction]
-fn lower(a: String) -> PyResult<String> {
-    let output = featurizers::lower(a);
+fn featurizer(input: Vec<String>) -> PyResult<Vec<String>> {
+    let output = featurizers::featurizer(input);
     Ok(output)
 }
 
 #[pymodule]
 fn underthesea_core(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    m.add_function(wrap_pyfunction!(lower, m)?)?;
+    m.add_function(wrap_pyfunction!(featurizer, m)?)?;
     Ok(())
 }
