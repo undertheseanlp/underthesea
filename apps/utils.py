55ac46746b92ea3aa5c3c8c10c85c0d5631e9773
Vu Anh <anhv.ict91@gmail.com>
2022-04-11 07:55:24 +0700

    update

--- apps/utils.py
+++
@@ -1,4 +0,0 @@
-from os.path import abspath, dirname, join
-
-PROJECT_FOLDER = dirname(dirname(abspath(__file__)))
-DICTIONARY_FILE = join(PROJECT_FOLDER, "datasets", "UD_Vietnamese-COL", "dictionary", "dictionary.yaml")
