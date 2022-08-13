8777cc1577b4a0980888b2969b820e819211068d
Vu Anh <anhv.ict91@gmail.com>
2022-08-14 02:15:17 +0700

    GH-530: update underthesea_core 0.0.5a2 (#555)

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = [
     'scikit-learn',
     'unidecode',
     'PyYAML',
-    'underthesea_core==0.0.4'
+    'underthesea_core==0.0.5a2'
 ]
 
 tests_require = [
@@ -68,6 +68,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
