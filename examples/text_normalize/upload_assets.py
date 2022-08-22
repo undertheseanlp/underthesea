9e98487fa54909e43d87121fc85041d4b305d8a0
Vu Anh <anhv.ict91@gmail.com>
2022-08-22 21:15:02 +0700

    update

---
+++ examples/text_normalize/upload_assets.py
@@ -0,0 +1,36 @@
+import os
+from os import listdir
+from os.path import join
+
+from github import Github
+import base64
+
+from underthesea.utils import logger
+
+access_token = os.environ['GITHUB_ACCESS_TOKEN']
+g = Github(access_token)
+repo = g.get_repo('undertheseanlp/underthesea')
+release = repo.get_release('open-data-voice-ipa')
+assets = {}
+for asset in release.get_assets():
+    name = asset.name
+    assets[name] = asset
+
+SOUNDS_FOLDER = join("outputs", "sound", "zalo")
+names = listdir(SOUNDS_FOLDER)
+
+
+def generate_id(word):
+    return "".join([hex(ord(c)) for c in word])
+
+
+for name in names:
+    path = join(SOUNDS_FOLDER, name)
+    word = name.split(".")[0]
+    word_id = generate_id(word)
+    asset_name = word_id + ".mp3"
+    logger.info(f'Upload file {asset_name} for word {name}')
+    if asset_name in assets:
+        asset = assets[asset_name]
+        asset.delete_asset()
+    release.upload_asset(path, name=asset_name)
