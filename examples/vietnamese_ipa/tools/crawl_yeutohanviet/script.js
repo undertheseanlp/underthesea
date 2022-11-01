e31624f3ebeb60f353965377f80bb9ffdb6e989b
Vu Anh <anhv.ict91@gmail.com>
2022-11-01 09:23:06 +0700

    update

---
+++ examples/vietnamese_ipa/tools/crawl_yeutohanviet/script.js
@@ -0,0 +1,33 @@
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto('https://developers.google.com/web/');
+
+  // Type into search box.
+  await page.type('.devsite-search-field', 'Headless Chrome');
+
+  // Wait for suggest overlay to appear and click "show all results".
+  const allResultsSelector = '.devsite-suggest-all-results';
+  await page.waitForSelector(allResultsSelector);
+  await page.click(allResultsSelector);
+
+  // Wait for the results page to load and display the results.
+  const resultsSelector = '.gsc-results .gs-title';
+  await page.waitForSelector(resultsSelector);
+
+  // Extract the results from the page.
+  const links = await page.evaluate(resultsSelector => {
+    return [...document.querySelectorAll(resultsSelector)].map(anchor => {
+      const title = anchor.textContent.split('|')[0].trim();
+      return `${title} - ${anchor.href}`;
+    });
+  }, resultsSelector);
+
+  // Print all the files.
+  console.log(links.join('\n'));
+
+  await browser.close();
+})();
