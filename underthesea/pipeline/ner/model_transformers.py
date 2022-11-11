c29a7deb0a0c8c054d26bba6547f82e03708024d
Vu Anh <anhv.ict91@gmail.com>
2022-11-11 08:37:44 +0700

    GH-437: Train NER model with VLSP 2016 NER syllable (#591)

--- underthesea/pipeline/ner/model_transformers.py
+++ underthesea/pipeline/ner/model_transformers.py
@@ -1,6 +1,6 @@
 from transformers import pipeline, AutoModelForTokenClassification, AutoTokenizer
 
-model_repo = "rain1024/underthesea_vlsp2016_ner"
+model_repo = "undertheseanlp/vietnamese-ner-v1.4.0a2"
 model_fine_tuned = AutoModelForTokenClassification.from_pretrained(model_repo)
 tokenizer = AutoTokenizer.from_pretrained(model_repo)
 nlp = pipeline("ner", model=model_fine_tuned, tokenizer=tokenizer)
@@ -8,4 +8,5 @@ nlp = pipeline("ner", model=model_fine_tuned, tokenizer=tokenizer)
 if __name__ == '__main__':
     example = "Chưa tiết lộ lịch trình tới Việt Nam của Tổng thống Mỹ Donald Trump"
     ner_results = nlp(example)
-    print(ner_results)
+    for entity in ner_results:
+        print(entity)
