7fbaa1a21f07efb9ff83b2579f8c0f6353b60fe7
Vu Anh <anhv.ict91@gmail.com>
2022-12-12 02:51:06 +0700

    update

--- apps/linguistic/frontend/src/features/articles/ArticleDetail.tsx
+++ apps/linguistic/frontend/src/features/articles/ArticleDetail.tsx
@@ -9,7 +9,7 @@ export function ArticleDetail() {
     <Container>
       <br />
       <h3>Đội hình xuất sắc nhất vòng tứ kết World Cup 2022: Messi sáng rực rỡ</h3>
-      <p>Siêu sao Lionel Messi đã lọt vào đội hình xuất sắc nhất vòng tứ kết World Cup 2022 theo bình chọn của tờ Sofa Score..</p>
+      <p>Siêu sao Lionel Messi đã lọt vào đội hình xuất sắc nhất vòng tứ kết World Cup 2022 theo bình chọn của tờ Sofa Score.</p>
     </Container>
   )
 }
