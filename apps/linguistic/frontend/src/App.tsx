f49606c01b1ba0ccde1af7a2c32c32a8b9c19fa4
Vu Anh <anhv.ict91@gmail.com>
2022-12-12 00:05:44 +0700

    update

--- apps/linguistic/frontend/src/App.tsx
+++ apps/linguistic/frontend/src/App.tsx
@@ -3,13 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Button, Alert, Breadcrumb } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Menu } from './components/Menu';
+// import { Menu } from './components/Menu';
 
 function App() {
   return (
     
     <div className="App">
-      <Menu></Menu>
+      {/* <Menu></Menu> */}
       <header className="App-header">
         <Alert variant="warning">This is an alert</Alert> 
         <Button>Test Button</Button>
