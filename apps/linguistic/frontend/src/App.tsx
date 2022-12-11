8491a2a64242dbfa3083309a88cec5ce828fb6fa
Vu Anh <anhv.ict91@gmail.com>
2022-12-11 16:39:58 +0700

    add linguistic

---
+++ apps/linguistic/frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button, Alert, Breadcrumb } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Menu } from './components/Menu';
+
+function App() {
+  return (
+    
+    <div className="App">
+      <Menu></Menu>
+      <header className="App-header">
+        <Alert variant="warning">This is an alert</Alert> 
+        <Button>Test Button</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ apps/linguistic/frontend/src/components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div>Menu</div>
+  )
+}
+
+export default Sidebar
