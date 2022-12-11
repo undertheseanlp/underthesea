f0593b8d3ad305d1b1ad22934db6f13a613709a0
Vu Anh <anhv.ict91@gmail.com>
2022-12-12 01:21:08 +0700

    add article page

--- apps/linguistic/frontend/src/App.tsx
+++ apps/linguistic/frontend/src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavbarMain } from './components/NavbarMain';
+import React from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { NavbarMain } from './components/NavbarMain'
+import { Article } from './components/Article'
 
 function App() {
   return (
-    <NavbarMain></NavbarMain>
-  );
+    <div>
+      <NavbarMain></NavbarMain>
+      <Article></Article>
+    </div>
+  )
 }
 
-export default App;
+export default App
