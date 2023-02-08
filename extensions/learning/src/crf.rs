b72f0ac8a60eaf4511960cd105b8d0e35608f064
Vu Anh <anhv.ict91@gmail.com>
2023-02-08 18:04:51 +0700

    update

--- extensions/learning/src/crf.rs
+++ extensions/learning/src/crf.rs
@@ -18,3 +18,19 @@ impl Student {
         self.classes.push(class);
     }
 }
+
+// create struct CRF
+struct CRF {
+}
+
+// implement method train for CRF
+impl CRF {
+    fn train(&mut self){
+        // do something
+    }
+
+    // implement method tag with parameter features
+    fn tag(&mut self, features: Vec<String>) -> Vec<String> {
+        vec![]
+    }
+}
---
+++ extensions/learning/tests/test_crf.rs
@@ -0,0 +1,9 @@
+// create test for CRF struct
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_crf() {
+        assert_eq!(1, 1);
+    }
+}
