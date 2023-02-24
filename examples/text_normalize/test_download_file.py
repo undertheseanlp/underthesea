dc9e39c3b53f6bb674ac5d2679b64425d149904a
Vu Anh <anhv.ict91@gmail.com>
2023-02-25 06:30:31 +0700

    update

--- examples/text_normalize/test_download_file.py
+++ examples/text_normalize/test_download_file.py
@@ -1,4 +1,4 @@
-if __name__ == '__main__':    
+if __name__ == "__main__":
     mp3_file_url = "https://chunk.lab.zalo.ai/ad2817ece88b01d5589a/ad2817ece88b01d5589a"
     output_file = "bay1.mp3"
     # r = requests.get(mp3_file_url, allow_redirects=True)
