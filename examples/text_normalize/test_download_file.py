6af708f001ab3bca1ffeb24d3f6eb2494717327a
Vu Anh <anhv.ict91@gmail.com>
2023-02-25 06:29:46 +0700

    update

--- examples/text_normalize/test_download_file.py
+++ examples/text_normalize/test_download_file.py
@@ -1,7 +1,6 @@
-import requests
-
-mp3_file_url = "https://chunk.lab.zalo.ai/ad2817ece88b01d5589a/ad2817ece88b01d5589a"
-output_file = "bay1.mp3"
-# r = requests.get(mp3_file_url, allow_redirects=True)
-# with open(output_file, 'wb') as f:
-#     f.write(r.content)
+if __name__ == '__main__':    
+    mp3_file_url = "https://chunk.lab.zalo.ai/ad2817ece88b01d5589a/ad2817ece88b01d5589a"
+    output_file = "bay1.mp3"
+    # r = requests.get(mp3_file_url, allow_redirects=True)
+    # with open(output_file, 'wb') as f:
+    # f.write(r.content)
