dba3f437e3acc02b8ffbb662c598cca4136dd92a
Vu Anh <anhv.ict91@gmail.com>
2023-03-02 07:18:48 +0700

    update

--- examples/pos_tagging/train_local.py
+++ examples/pos_tagging/train_local.py
@@ -24,7 +24,9 @@ features = [
     # word unigram and bigram and trigram
     "T[-2]", "T[-1]", "T[0]", "T[1]", "T[2]",
     "T[-2,-1]", "T[-1,0]", "T[0,1]", "T[1,2]",
-    "T[-2,0]", "T[-1,1]", "T[0,2]"
+    "T[-2,0]", "T[-1,1]", "T[0,2]",
+    "T[-2].lower", "T[-1].lower", "T[0].lower", "T[1].lower", "T[2].lower",
+    "T[0].istitle", "T[-1].istitle", "T[1].istitle",
 ]
 model = FastCRFSequenceTagger(features, dictionary)
 
