88ebab0c68b69b5c4035e17bffc8954c0ca2ef1a
Vu Anh <anhv.ict91@gmail.com>
2023-03-02 09:30:22 +0700

    update

--- examples/ner/predict_crf.py
+++ examples/ner/predict_crf.py
@@ -1,4 +1,4 @@
-#@title Predict
+import hydra
 
 from os.path import dirname, join
 from underthesea.models.fast_crf_sequence_tagger import FastCRFSequenceTagger
@@ -6,13 +6,27 @@ from underthesea import word_tokenize
 from underthesea import pos_tag
 
 working_dir = dirname(__file__)
-output_dir = join(working_dir, "tmp/ner_20220303")
-# sentence = "Quỳnh Như tiết lộ với báo Bồ Đào Nha về hành trình làm nên lịch sử" #@param {type: "string"}
-sentence = "Quỳnh Như tiết lộ với báo Bồ Đào Nha về hành trình làm nên lịch sử" #@param {type: "string"}
-tokens = pos_tag(sentence)
 
-model = FastCRFSequenceTagger()
-model.load(output_dir)
-y = model.predict(tokens)
-for token, x in zip(tokens, y):
-    print(token, "\t", x)
+@hydra.main
+def my_app(cfg):
+    if cfg["output_dir"] is None:
+        output_dir_path = "tmp/ner" 
+    else:
+        output_dir_path = cfg["output_dir"]
+    output_dir = join(working_dir, output_dir_path)
+
+    if cfg["text"] is None:
+        text = "Quỳnh Như tiết lộ với báo Bồ Đào Nha về hành trình làm nên lịch sử"
+    else:
+        text = cfg["text"]
+
+    tokens = pos_tag(text)
+
+    model = FastCRFSequenceTagger()
+    model.load(output_dir)
+    y = model.predict(tokens)
+    for token, x in zip(tokens, y):
+        print(token, "\t", x)
+
+if __name__ == '__main__':
+    my_app()
