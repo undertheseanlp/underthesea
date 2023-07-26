fb9c8c6b0db682ba6eb902fbd9b02fd4d3370b22
Vu Anh <anhv.ict91@gmail.com>
2023-07-26 22:23:23 +0700

    update

--- examples/word_tokenize/README.md
+++ examples/word_tokenize/README.md
@@ -2,3 +2,27 @@
 
 * [Google Colab](https://colab.research.google.com/drive/1NR9NlHJDj5_wywRze7yQizw1DgICKL72?usp=sharing)
 * [Technical Report](technical_report.md)
+
+## Usage
+
+### Training
+
+Train and Evaluate Model:
+
+```
+python train.py ++'dataset.include_test=False'
+```
+
+Train the Final Model (Including Test Split):
+
+```
+python train.py ++'dataset.include_test=True'
+```
+
+### Inference
+
+Generate labels with the trained model
+
+```
+python predict.py +'output_dir="tmp/ws_20230725/"' +'text="Chưa tiết lộ lịch trình tới Việt Nam của Tổng thống Mỹ Donald Trump"'
+```
--- examples/word_tokenize/train.py
+++ examples/word_tokenize/train.py
@@ -1,61 +1,38 @@
-from os.path import dirname, join
+import hydra
+from hydra.utils import get_original_cwd
+from omegaconf import DictConfig, OmegaConf
+
+from os.path import join
 from underthesea.models.fast_crf_sequence_tagger import FastCRFSequenceTagger
 from underthesea.trainers.crf_trainer import CRFTrainer
 from underthesea.transformer.tagged_feature import lower_words as dictionary
 from datasets import load_dataset
 from underthesea.utils.preprocess_dataset import preprocess_word_tokenize_dataset
 
-def train():
+
+@hydra.main(version_base=None, config_path="conf/", config_name="config")
+def train(cfg: DictConfig) -> None:
+    wd = get_original_cwd()
+    print(OmegaConf.to_yaml(cfg))
+
     features = [
         # word unigram and bigram and trigram
-        "T[-2]",
-        "T[-1]",
-        "T[0]",
-        "T[1]",
-        "T[2]",
-        "T[-2,-1]",
-        "T[-1,0]",
-        "T[0,1]",
-        "T[1,2]",
-        "T[-2,0]",
-        "T[-1,1]",
-        "T[0,2]",
-        "T[-2].lower",
-        "T[-1].lower",
-        "T[0].lower",
-        "T[1].lower",
-        "T[2].lower",
-        "T[-2,-1].lower",
-        "T[-1,0].lower",
-        "T[0,1].lower",
-        "T[1,2].lower",
-        "T[-1].isdigit",
-        "T[0].isdigit",
-        "T[1].isdigit",
-        "T[-2].istitle",
-        "T[-1].istitle",
-        "T[0].istitle",
-        "T[1].istitle",
-        "T[2].istitle",
-        "T[0,1].istitle",
-        "T[0,2].istitle",
-        "T[-2].is_in_dict",
-        "T[-1].is_in_dict",
-        "T[0].is_in_dict",
-        "T[1].is_in_dict",
-        "T[2].is_in_dict",
-        "T[-2,-1].is_in_dict",
-        "T[-1,0].is_in_dict",
-        "T[0,1].is_in_dict",
-        "T[1,2].is_in_dict",
-        "T[-2,0].is_in_dict",
-        "T[-1,1].is_in_dict",
-        "T[0,2].is_in_dict",
+        "T[-2]", "T[-1]", "T[0]", "T[1]", "T[2]",
+        "T[-2,-1]", "T[-1,0]", "T[0,1]", "T[1,2]", "T[-2,0]",
+        "T[-1,1]", "T[0,2]",
+        "T[-2].lower", "T[-1].lower", "T[0].lower", "T[1].lower", "T[2].lower",
+        "T[-2,-1].lower", "T[-1,0].lower", "T[0,1].lower", "T[1,2].lower",
+        "T[-1].isdigit", "T[0].isdigit", "T[1].isdigit",
+        "T[-2].istitle", "T[-1].istitle", "T[0].istitle", "T[1].istitle", "T[2].istitle",
+        "T[0,1].istitle", "T[0,2].istitle",
+        "T[-2].is_in_dict", "T[-1].is_in_dict", "T[0].is_in_dict", "T[1].is_in_dict", "T[2].is_in_dict",
+        "T[-2,-1].is_in_dict", "T[-1,0].is_in_dict",
+        "T[0,1].is_in_dict", "T[1,2].is_in_dict", "T[-2,0].is_in_dict",
+        "T[-1,1].is_in_dict", "T[0,2].is_in_dict",
     ]
     model = FastCRFSequenceTagger(features, dictionary)
 
-    pwd = dirname(__file__)
-    output_dir = join(pwd, "tmp/ws_20220224")
+    output_dir = join(wd, "tmp/ws_20230726")
     training_params = {
         "output_dir": output_dir,
         "params": {
@@ -68,7 +45,6 @@ def train():
         },
     }
 
-
     dataset = load_dataset("undertheseanlp/UTS_WTK", "large", revision="1.0")
     corpus = preprocess_word_tokenize_dataset(dataset)
 
@@ -79,6 +55,7 @@ def train():
 
     trainer = CRFTrainer(model, training_params, train_dataset, test_dataset)
     trainer.train()
+
 
 if __name__ == "__main__":
     train()
