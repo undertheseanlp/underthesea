81d2d788938ebdb9c82662fe629a611ceed03895
Vu Anh <anhv.ict91@gmail.com>
2023-07-26 22:05:37 +0700

    update

---
+++ examples/word_tokenize/technical_report.md
@@ -0,0 +1,11 @@
+# Technical Report 
+
+Vietnamese Word Segmentation (VWS) plays a pivotal role in various Natural Language Processing (NLP) tasks for the Vietnamese language. Segmentation can be thought of as the process of splitting a sequence of characters into meaningful chunks or "words." This is particularly challenging for Vietnamese due to its nature of combining multiple lexical units into single written forms, which can be confusing without proper context. Over the past years, numerous models have been proposed to tackle this issue, with Conditional Random Fields (CRF) being one of the prominent ones due to its ability to consider the context for making segmentation decisions. This report presents our experiment leveraging the CRF model for the VWS task on the UTS_WTK dataset.
+
+## Results
+
+The table below captures the results of the Vietnamese Word Segmentation task using the Conditional Random Fields (CRF) model:
+
+| Dataset         | F1 Score |
+|----------------:|---------:|
+| UTS_WTK (1.0.0) | 0.977    |
--- examples/word_tokenize/train.py
+++ examples/word_tokenize/train.py
@@ -5,77 +5,80 @@ from underthesea.transformer.tagged_feature import lower_words as dictionary
 from datasets import load_dataset
 from underthesea.utils.preprocess_dataset import preprocess_word_tokenize_dataset
 
-features = [
-    # word unigram and bigram and trigram
-    "T[-2]",
-    "T[-1]",
-    "T[0]",
-    "T[1]",
-    "T[2]",
-    "T[-2,-1]",
-    "T[-1,0]",
-    "T[0,1]",
-    "T[1,2]",
-    "T[-2,0]",
-    "T[-1,1]",
-    "T[0,2]",
-    "T[-2].lower",
-    "T[-1].lower",
-    "T[0].lower",
-    "T[1].lower",
-    "T[2].lower",
-    "T[-2,-1].lower",
-    "T[-1,0].lower",
-    "T[0,1].lower",
-    "T[1,2].lower",
-    "T[-1].isdigit",
-    "T[0].isdigit",
-    "T[1].isdigit",
-    "T[-2].istitle",
-    "T[-1].istitle",
-    "T[0].istitle",
-    "T[1].istitle",
-    "T[2].istitle",
-    "T[0,1].istitle",
-    "T[0,2].istitle",
-    "T[-2].is_in_dict",
-    "T[-1].is_in_dict",
-    "T[0].is_in_dict",
-    "T[1].is_in_dict",
-    "T[2].is_in_dict",
-    "T[-2,-1].is_in_dict",
-    "T[-1,0].is_in_dict",
-    "T[0,1].is_in_dict",
-    "T[1,2].is_in_dict",
-    "T[-2,0].is_in_dict",
-    "T[-1,1].is_in_dict",
-    "T[0,2].is_in_dict",
-]
-model = FastCRFSequenceTagger(features, dictionary)
+def train():
+    features = [
+        # word unigram and bigram and trigram
+        "T[-2]",
+        "T[-1]",
+        "T[0]",
+        "T[1]",
+        "T[2]",
+        "T[-2,-1]",
+        "T[-1,0]",
+        "T[0,1]",
+        "T[1,2]",
+        "T[-2,0]",
+        "T[-1,1]",
+        "T[0,2]",
+        "T[-2].lower",
+        "T[-1].lower",
+        "T[0].lower",
+        "T[1].lower",
+        "T[2].lower",
+        "T[-2,-1].lower",
+        "T[-1,0].lower",
+        "T[0,1].lower",
+        "T[1,2].lower",
+        "T[-1].isdigit",
+        "T[0].isdigit",
+        "T[1].isdigit",
+        "T[-2].istitle",
+        "T[-1].istitle",
+        "T[0].istitle",
+        "T[1].istitle",
+        "T[2].istitle",
+        "T[0,1].istitle",
+        "T[0,2].istitle",
+        "T[-2].is_in_dict",
+        "T[-1].is_in_dict",
+        "T[0].is_in_dict",
+        "T[1].is_in_dict",
+        "T[2].is_in_dict",
+        "T[-2,-1].is_in_dict",
+        "T[-1,0].is_in_dict",
+        "T[0,1].is_in_dict",
+        "T[1,2].is_in_dict",
+        "T[-2,0].is_in_dict",
+        "T[-1,1].is_in_dict",
+        "T[0,2].is_in_dict",
+    ]
+    model = FastCRFSequenceTagger(features, dictionary)
 
-pwd = dirname(__file__)
-output_dir = join(pwd, "tmp/ws_20220224")
-training_params = {
-    "output_dir": output_dir,
-    "params": {
-        "c1": 1.0,  # coefficient for L1 penalty
-        "c2": 1e-3,  # coefficient for L2 penalty
-        "max_iterations": 1000,  #
-        # include transitions that are possible, but not observed
-        "feature.possible_transitions": True,
-        "feature.possible_states": True,
-    },
-}
+    pwd = dirname(__file__)
+    output_dir = join(pwd, "tmp/ws_20220224")
+    training_params = {
+        "output_dir": output_dir,
+        "params": {
+            "c1": 1.0,  # coefficient for L1 penalty
+            "c2": 1e-3,  # coefficient for L2 penalty
+            "max_iterations": 1000,  #
+            # include transitions that are possible, but not observed
+            "feature.possible_transitions": True,
+            "feature.possible_states": True,
+        },
+    }
 
 
-dataset = load_dataset("undertheseanlp/UTS_WTK", "base")
-corpus = preprocess_word_tokenize_dataset(dataset)
+    dataset = load_dataset("undertheseanlp/UTS_WTK", "large", revision="1.0")
+    corpus = preprocess_word_tokenize_dataset(dataset)
 
-train_dataset = corpus["train"]
-test_dataset = corpus["test"]
-print("Train dataset", len(train_dataset))
-print("Test dataset", len(test_dataset))
+    train_dataset = corpus["train"]
+    test_dataset = corpus["test"]
+    print("Train dataset", len(train_dataset))
+    print("Test dataset", len(test_dataset))
 
-trainer = CRFTrainer(model, training_params, train_dataset, test_dataset)
+    trainer = CRFTrainer(model, training_params, train_dataset, test_dataset)
+    trainer.train()
 
-trainer.train()
+if __name__ == "__main__":
+    train()
