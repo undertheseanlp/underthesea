49ac26d67fedafe8ac123986b7697483238c9181
Vu Anh <anhv.ict91@gmail.com>
2023-07-27 14:03:14 +0700

    add simple module

--- underthesea/pipeline/classification/__init__.py
+++ underthesea/pipeline/classification/__init__.py
@@ -1,29 +1,37 @@
 # -*- coding: utf-8 -*-
 
 
-def classify(X, domain=None):
+def classify(X, domain=None, model=None):
     """
     Text classification
 
-    Parameters
-    ==========
+    Args:
+        X (str): The raw sentence
+        domain (str, optional): The domain of the text. Defaults to None.
+            Options include:
+                - None: general domain
+                - 'bank': bank domain
+        model (str, optional): The classification model. Defaults to None.
+            Options include:
+                - None: default underthesea classifier
+                - 'prompt': OpenAI prompt model
 
-    X: {unicode, str}
-        raw sentence
-    domain: {None, 'bank'}
-        domain of text
-            * None: general domain
-            * bank: bank domain
-    Returns
-    =======
-    tokens: list
-        categories of sentence
-
+    Returns:
+        list: A list containing the categories of the sentence
     """
     if X == "":
         return None
+
+    if model == 'prompt':
+        from underthesea.pipeline.classification import classification_prompt
+        args = {
+            "domain": domain
+        }
+        return classification_prompt.classify(X, **args)
+
     if domain == 'bank':
         from underthesea.pipeline.classification import bank
         return bank.classify(X)
+
     from underthesea.pipeline.classification import vntc
     return vntc.classify(X)
