df8b2a40a30536ae60eaa5776b6062585641de70
Vu Anh <anh.vu2@mservice.com.vn>
2023-09-30 20:42:08 +0700

    update

--- datasets/CP_Vietnamese-VLC/scripts/scrapers/test_thuvienphapluat_scraper.py
+++ datasets/CP_Vietnamese-VLC/scripts/scrapers/test_thuvienphapluat_scraper.py
@@ -17,11 +17,16 @@ class TestThuvienphapluatScraper(unittest.TestCase):
     def test_scrape_1(self):
         url = self.read_testdata("1.in")
         result = self.scrapper.scrape(url)
-        # cwd = os.path.dirname(__file__)
-        # with open(os.path.join(cwd, "out.txt"), "w") as f:
-        #     f.write(result)
         expected = self.read_testdata("1.out")
         self.assertEqual(result, expected)
+
+    def test_scrape_list_page_1(self):
+        url = "https://thuvienphapluat.vn/page/tim-van-ban.aspx?keyword=&area=0&type=10&status=0&lan=1&org=0&signer=0&match=True&sort=1&bdate=26/09/1943&edate=27/09/2023"
+        result = self.scrapper.scrape_list_page(url)
+        self.assertTrue(len(result) > 10)
+        first_item = result[0]
+        self.assertTrue("title" in first_item)
+        self.assertTrue("url" in first_item)
 
 
 if __name__ == '__main__':
--- datasets/CP_Vietnamese-VLC/scripts/scrapers/thuvienphapluat_scraper.py
+++ datasets/CP_Vietnamese-VLC/scripts/scrapers/thuvienphapluat_scraper.py
@@ -48,3 +48,57 @@ class ThuvienphapluatScraper:
         # remove spaces before and after newlines
         content = re.sub(r'\s*\n\s*', '\n', content)
         return content
+    
+
+    def scrape_article_page(self, url: str) -> str:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        parent_element = soup.find(class_="cldivContentDocVn")
+
+        # Find the child element with the class "content1" within the parent element
+        child_element = parent_element.find(class_="content1")
+
+        content = ""
+        paragraphs = child_element.find_all("p")
+        for p in paragraphs:
+            text = p.get_text() + "\n"
+            text = text.replace("\r\n", " ")
+            text = text.strip()
+            content += text + "\n"
+        content = content.strip()
+
+        # remove any two or more consecutive newlines
+        content = re.sub(r'\n{2,}', '\n', content)
+
+        # remove any two or more consecutive spaces
+        content = re.sub(r' {2,}', ' ', content)
+
+        # remove any two or more consecutive tabs
+        content = re.sub(r'\t{2,}', '\t', content)
+        # remove spaces before and after newlines
+        content = re.sub(r'\s*\n\s*', '\n', content)
+        return content
+    
+
+    def scrape_directory_page(self, url: str) -> dict:
+        response = requests.get(url)
+        
+        # Check if the request was successful
+        if response.status_code != 200:
+            response.raise_for_status()
+        
+        soup = BeautifulSoup(response.content, "html.parser")
+        
+        article_pages = []
+        directory_urls = []
+        
+        # Find all the <a> tags with the 'onclick' attribute set to 'Doc_CT(MemberGA)'
+        for a_tag in soup.find_all('a', onclick='Doc_CT(MemberGA)'):
+            title = a_tag.get_text(strip=True)  # Get the text content of the <a> tag (the title of the article)
+            url = a_tag.get('href')  # Get the href attribute of the <a> tag (the URL of the article)
+            
+            # Append the title and URL to the list in the requested structure
+            article_pages.append({'title': title, 'url': url})
+        
+
+        return article_pages, directory_urls
