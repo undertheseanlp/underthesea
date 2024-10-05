bc8b861f3da75805c42cb490bc2a6632e791ac5e
Vu Anh <anhv.ict91@gmail.com>
2024-10-05 23:46:56 +0000

    update

---
+++ apps/languagesv2/languages-v2/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Languages v2
+      </header>
+    </div>
+  );
+}
+
+export default App;
