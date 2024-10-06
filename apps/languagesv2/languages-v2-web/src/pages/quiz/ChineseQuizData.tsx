6f017d17e99e7114c0747753997e7fc03f18c438
Vu Anh <anhv.ict91@gmail.com>
2024-10-06 02:47:16 +0000

    update

---
+++ apps/languagesv2/languages-v2-web/src/pages/quiz/ChineseQuizData.tsx
@@ -0,0 +1,50 @@
+const ChineseQuizData = [
+    {
+      questionText: '“爱” (ài) 的意思是什么?',
+      answerOptions: [
+        { answerText: '爱', isCorrect: true },
+        { answerText: '水', isCorrect: false },
+        { answerText: '火', isCorrect: false },
+        { answerText: '树', isCorrect: false },
+      ],
+    },
+    {
+      questionText: '“水” (shuǐ) 的意思是什么?',
+      answerOptions: [
+        { answerText: '水', isCorrect: true },
+        { answerText: '火', isCorrect: false },
+        { answerText: '树', isCorrect: false },
+        { answerText: '爱', isCorrect: false },
+      ],
+    },
+    {
+      questionText: '“火” (huǒ) 的意思是什么?',
+      answerOptions: [
+        { answerText: '火', isCorrect: true },
+        { answerText: '水', isCorrect: false },
+        { answerText: '树', isCorrect: false },
+        { answerText: '爱', isCorrect: false },
+      ],
+    },
+    {
+      questionText: '“大” (dà) 的意思是什么?',
+      answerOptions: [
+        { answerText: '大', isCorrect: true },
+        { answerText: '小', isCorrect: false },
+        { answerText: '高', isCorrect: false },
+        { answerText: '矮', isCorrect: false },
+      ],
+    },
+    {
+      questionText: '“小” (xiǎo) 的意思是什么?',
+      answerOptions: [
+        { answerText: '小', isCorrect: true },
+        { answerText: '大', isCorrect: false },
+        { answerText: '高', isCorrect: false },
+        { answerText: '矮', isCorrect: false },
+      ],
+    },
+  ];
+  
+  export default ChineseQuizData;
+  
