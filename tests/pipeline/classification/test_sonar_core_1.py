3e8ea4dfd815c5eab57fd9310f90e2eeab6ee34b
Vu Anh <anhv.ict91@gmail.com>
2025-09-21 17:38:10 +0700

    GH-731: Update output format of model sonar_core_1 (#815)

--- tests/pipeline/classification/test_sonar_core_1.py
+++ tests/pipeline/classification/test_sonar_core_1.py
@@ -12,30 +12,30 @@ class TestSonarCore1Classify(TestCase):
 
     def test_classify_simple_case(self):
         text = u"HLV ngoại đòi gần tỷ mỗi tháng dẫn dắt tuyển Việt Nam"
-        actual = classify(text)[0]
+        actual = classify(text)
         expected = "the_thao"
         self.assertEqual(actual, expected)
 
     def test_classify_sports(self):
         text = u"Việt Nam giành chiến thắng 3-0 trước Thái Lan trong trận bán kết"
-        actual = classify(text)[0]
+        actual = classify(text)
         expected = "the_thao"
         self.assertEqual(actual, expected)
 
     def test_classify_technology(self):
         text = u"Apple ra mắt iPhone mới với nhiều tính năng đột phá"
-        actual = classify(text)[0]
+        actual = classify(text)
         expected = "vi_tinh"
         self.assertEqual(actual, expected)
 
     def test_classify_health(self):
         text = u"Phát hiện vaccine mới chống lại virus corona"
-        actual = classify(text)[0]
+        actual = classify(text)
         expected = "suc_khoe"
         self.assertEqual(actual, expected)
 
     def test_classify_business(self):
         text = u"Thị trường chứng khoán tăng điểm mạnh trong phiên sáng nay"
-        actual = classify(text)[0]
+        actual = classify(text)
         expected = "kinh_doanh"
         self.assertEqual(actual, expected)
